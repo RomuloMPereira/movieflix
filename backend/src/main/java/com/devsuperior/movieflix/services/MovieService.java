7922580201d39bd8f7fa0680ed20b23af8cdc7bd
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-07-06 16:21:24 -0300

    Endpoint GET '/movies' ok (returning a page of movies ordered by title and filtered by genre)/ All tests related to MovieResource have been successfully run

--- backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -3,11 +3,15 @@ package com.devsuperior.movieflix.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.repositories.MovieRepository;
 import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
@@ -17,10 +21,20 @@ public class MovieService {
 	@Autowired
 	private MovieRepository repository;
 	
+	@Autowired
+	private GenreRepository genreRepository;
+	
 	@Transactional(readOnly = true)
 	public MovieDTO findById(Long id) {
 		Optional<Movie> optional = repository.findById(id);
 		Movie movie = optional.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new MovieDTO(movie, movie.getReviews());
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAllPaged(Long genreId, PageRequest pageRequest){
+		Genre genre = (genreId == 0) ? null : genreRepository.getOne(genreId);
+		Page<Movie> page = repository.find(genre, pageRequest);
+		return page.map(movie -> new MovieDTO(movie));
 	}
 }
