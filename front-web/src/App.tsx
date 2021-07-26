104ccf0cad156fc94abe6c9982f1380092a3584e
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-07-26 15:49:36 -0300

    Navbar layout

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './core/assets/styles/custom.scss';
 import './App.scss';
+import Navbar from 'core/components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      Olá mundo!
-    </div>
+    <Navbar />
   );
 }
 
---
+++ front-web/src/core/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+const Navbar = () => {
+    return (
+        <div className="main-nav bg-primary">
+            <a href="#home" className="nav-log-text">
+                <h4>MovieFlix</h4>
+            </a>
+        </div>
+    );
+}
+
+export default Navbar;
