e8dbf0387b40cc7e9c9f788b1bafdc6b9ea8c625
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-07-26 15:07:28 -0300

    Create React App/ Add Bootstrap and Node-SASS

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './core/assets/styles/custom.scss';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Olá mundo!
+    </div>
+  );
+}
+
+export default App;
