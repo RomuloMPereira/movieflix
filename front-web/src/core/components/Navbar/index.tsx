b6bf42882810bae13bcbf62e41608332f063b4b9
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-07-28 14:55:35 -0300

    Add link and logout to Navbar

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import { getAccessTokenDecoded, logout } from 'core/utils/auth';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './styles.scss';
 
 const Navbar = () => {
+    const [currentUser, setCurrentUser] = useState('');
+    const location = useLocation();
+
+    useEffect(() => {
+        const currentUserData = getAccessTokenDecoded();
+        setCurrentUser(currentUserData.user_name);
+    }, [location]);
+
+    const handleLogout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        event.preventDefault();
+        logout();
+    }
+
     return (
-        <div className="main-nav bg-primary">
-            <a href="#home" className="nav-log-text">
+        <nav className="main-nav bg-primary">
+            <Link to="/" className="nav-logo-text">
                 <h4>MovieFlix</h4>
-            </a>
-        </div>
+            </Link>
+            {currentUser && (
+                <a
+                    href="#logout"
+                    className="nav-link"
+                    onClick={(e) => {
+                        handleLogout(e);
+                    }}
+                >
+                    SAIR
+                </a>
+            )}
+        </nav>
     );
 }
 
